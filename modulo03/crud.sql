1b7adc8cbb6681e1934c35a34d575aa01718735a
Franciel Abreu <francielabreu23@gmail.com>
2022-06-09 23:05:25 -0300

    criação dos bancos de dados e definição dos comandos para criação,edição listagem e exclusão das tabelas

---
+++ modulo03/crud.sql
@@ -0,0 +1,11 @@
+USE db_escola;
+
+-- inserir 1 registro --
+INSERT INTO tb_professor (nome,email,cpf) VALUES('chiquim da tapiocas','chiquim@email.com','12345678912');
+
+-- inserir mais de 1 registro --
+INSERT INTO tb_professor (nome,email,cpf) 
+VALUES
+('joao da tapiocas','joao@email.com','12345678911');
+('ze da tapiocas','ze@email.com','12345678913');
+('maria da tapiocas','maria@email.com','12345678912');
--- modulo03/db_escola.sql
+++ modulo03/db_escola.sql
@@ -31,3 +31,23 @@ DROP TABLE tb_professor;
 
 -- SElecionar dados da tabela --
 SELECT * FROM tb_professor;
+
+-- SElecionar dados especifico --
+SELECT * FROM tb_professor WHERE id='6';
+
+-- inserir 1 registro --
+INSERT INTO tb_professor (nome,email,cpf) VALUES('chiquim da tapiocas','chiquim@email.com','12345678912');
+
+-- inserir mais de 1 registro --
+INSERT INTO tb_professor (nome,email,cpf) 
+VALUES
+('joao da tapiocas','joao@email.com','12345678911');
+('ze da tapiocas','ze@email.com','12345678913');
+('maria da tapiocas','maria@email.com','12345678912');
+
+-- deletar todos --
+
+DELETE FROM tb_professor;
+
+-- editar dados de 1 registro --
+UPDATE tb_professor SET nome='Luiza da caucaia' WHERE cpf= '12345636734';
