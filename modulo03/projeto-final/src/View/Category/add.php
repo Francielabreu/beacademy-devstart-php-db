fff0da294c7b863bcf084452cb297fc04667816f
Franciel Abreu <francielabreu23@gmail.com>
2022-06-13 23:02:11 -0300

    alteração no codigo de adiconar categorias

---
+++ modulo03/projeto-final/src/View/Category/add.php
@@ -0,0 +1,13 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<h1>Adicionar categoria</h1>
+
+<form action="" method="post">
+
+    <label for="name">Nome</label>
+    <input class="form-control mb-3" type="text" name="name" id="name" >
+
+    <label for="description">Descrição</label>
+    <textarea class="form-control mb-3" name="description" id="description"></textarea>
+
+    <button class="bt btn-primary mb-3" type="submit">Enviar</button>
+</form>
