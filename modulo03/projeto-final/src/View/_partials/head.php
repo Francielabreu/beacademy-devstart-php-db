18ca28a04f29f484267016be445fb48c2fd84fbd
Franciel Abreu <francielabreu23@gmail.com>
2022-06-14 22:25:45 -0300

    creação da header e do footer para uma melhor qualidade de codigo e manutenção

---
+++ modulo03/projeto-final/src/View/_partials/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>E-commercePay</title>
+</head>
+
+<body class="container" style="background-color: #EFEFEF;">
+
+    <nav class="mt-3">
+        <a href="/" class="btn btn-outline-dark">Inicio</a>
+        <a href="/categorias" class="btn btn-outline-dark">Categorias</a>
+        <a href="/produtos" class="btn btn-outline-dark">Produtos</a>
+    </nav>
+
+    <hr>
