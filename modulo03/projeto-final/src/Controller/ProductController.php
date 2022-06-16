11f62e6cc2601a11345ccf1b112ddee111ea2365
Franciel Abreu <francielabreu23@gmail.com>
2022-06-16 19:28:28 -0300

    implementação da action de gerar Relatorio concluido

--- modulo03/projeto-final/src/Controller/ProductController.php
+++ modulo03/projeto-final/src/Controller/ProductController.php
@@ -3,25 +3,28 @@
 declare(strict_types=1);
 
 namespace App\Controller;
+
 use App\Connection\Connection;
+use Dompdf\Dompdf;
 
 
-class ProductController extends AbstractController{
+class ProductController extends AbstractController
+{
 
-    public function listAction():void
+    public function listAction(): void
     {
-        $con = Connection::getConnection(); 
-        $result =$con->prepare("SELECT * FROM tb_product");
+        $con = Connection::getConnection();
+        $result = $con->prepare("SELECT * FROM tb_product");
         $result->execute();
 
-        parent::render('Product/list',$result);
+        parent::render('Product/list', $result);
     }
 
 
 
-    public function addAction():void
+    public function addAction(): void
     {
-        $con = Connection::getConnection(); 
+        $con = Connection::getConnection();
 
         if ($_POST) {
             $name = $_POST['name'];
@@ -30,7 +33,7 @@ class ProductController extends AbstractController{
             $valor = $_POST['valor'];
             $quantity = $_POST['quantity'];
             $category_id = $_POST['category'];
-            $created_at= date('Y-m-d H:i:s');
+            $created_at = date('Y-m-d H:i:s');
 
             $query = $con->prepare("INSERT INTO tb_product (name,description,photo,valor,quantity,category_id,created_at) 
             VALUES
@@ -39,23 +42,19 @@ class ProductController extends AbstractController{
             $query->execute();
 
             parent::renderMessage('Produto Cadastrado com sucesso');
-            
-  
         }
         $result = $con->prepare("SELECT * FROM tb_category");
         $result->execute();
-        parent::render('Product/add',$result);
-        
-       
+        parent::render('Product/add', $result);
     }
 
 
 
-    public function editAction():void
+    public function editAction(): void
     {
-        $con = Connection::getConnection(); 
+        $con = Connection::getConnection();
 
-        
+
 
         $id = $_GET['id'];
         $product = $con->prepare("SELECT * FROM tb_product WHERE id='{$id}'");
@@ -82,32 +81,67 @@ class ProductController extends AbstractController{
             $resultUpdate = $con->prepare($query);
             $resultUpdate->execute();
             parent::renderMessage('Produto Atualizado com sucesso');
-           
         }
 
-        parent::render('Product/edit',[
-            'product'=>$product->fetch(\PDO::FETCH_ASSOC),
+        parent::render('Product/edit', [
+            'product' => $product->fetch(\PDO::FETCH_ASSOC),
         ]);
-
-        
     }
 
 
 
-    public function removeAction():void
+    public function removeAction(): void
     {
         $id = $_GET['id'];
         $con = Connection::getConnection();
-        $result =$con->prepare("DELETE FROM tb_product WHERE id='{$id}'");
+        $result = $con->prepare("DELETE FROM tb_product WHERE id='{$id}'");
         $result->execute();
 
-        
+
         parent::renderMessage('Produto Excluido com sucesso');
     }
 
 
-    public function reportAction():void
+    public function reportAction(): void
     {
-        $html = "<h1>Relatorio</h1>";
+        $con = Connection::getConnection();
+
+        $result = $con->prepare('SELECT prod.id,prod.name,prod.quantity,cat.name as category FROM tb_product prod INNER JOIN tb_category cat ON prod.category_id=cat.id');
+        $result->execute();
+
+        $content = '';
+        while ($product = $result->fetch(\PDO::FETCH_ASSOC)) {
+            extract($product);
+            $content .= "
+                <tr>{$id}</tr>
+                <tr>{$name}</tr>
+                <tr>{$quantity}</tr>
+            ";
+        }
+
+        $html = "
+            <h1>Relatorio de Produto no Estoque</h1>
+                <table border='1' width='100%'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOME</th>
+                            <th>QUANTIDADE</th>
+                        </tr>
+                    </thead>
+                     <tbody>
+                        {$content}
+                    </tbody>
+                 </table>
+            ";
+
+
+
+
+
+        $pdf = new Dompdf();
+        $pdf->loadHtml($html);
+        $pdf->render();
+        $pdf->stream();
     }
 }
